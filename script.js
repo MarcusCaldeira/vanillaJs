fb116422aba0c79c998028f7c048380902014a37
Marcus Caldeira <mac.caldr@gmail.com>
2018-05-08 18:56:17 -0600

    ok im tired for the night, and I think this my might be my last commit.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+/*
 var name = 'JOhn';
 console.log(name);
 
@@ -10,3 +12,35 @@ console.log(age);
 
 var fullAge = true;
 console.log(fullAge);
+*/
+
+// Lecture Variables 2
+
+var name = ('John');
+var age = 26;
+
+// console.log(name + age);
+
+var job, ismarried;
+// console.log(job);
+
+job = 'teacher';
+ismarried = 'false';
+console.log(name + ' is ' + age + ' years old ' + job + ' is he married? ' +
+ismarried + ' . ');
+
+age = 'thirty-five';
+job = 'driver';
+
+job = 'driver';
+ismarried = 'false';
+console.log(name + ' is ' + age + ' years old ' + job + ' is he married? ' +
+    ismarried + ' . ');
+
+// var lastName = prompt ('what is the last name?');
+// console.log(lastName);
+
+alert(name + ' is ' + age + ' years old ' + job + ' is he married? ' +
+    ismarried + ' . ');
+
+
