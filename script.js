b148325092895733651c526b157da10f5bdbd669
Marcus Caldeira <mac.caldr@gmail.com>
2018-05-16 12:52:25 -0600

    this is a function

--- script.js
+++ script.js
@@ -172,7 +172,20 @@ function calculateAge(yearOfBirth){
     var age = 2016 -yearOfBirth;
     return age;
 }
-calculateAge(1990);
+var ageJohn = calculateAge(1990);
+var ageMike = calculateAge(1969);
+var ageMary = calculateAge(1948);
+console.log(ageJohn);
+console.log(ageMike);
+console.log(ageMary);
+
+
+function yearsUntilRetirement(name, yearOfBirth){
+    var age = calculateAge(yearOfBirth);
+
+}
+yearsUntilRetirement('John', 1990);
+
 
 
 
