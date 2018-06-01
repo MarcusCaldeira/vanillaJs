d99fef81b5610c5d1c8e52ffd1528ce869b9f5f0
Marcus Caldeira <mac.caldr@gmail.com>
2018-06-01 08:02:18 -0600

    ok it worked now, now how to dispense a true false array if the person is in fact the adult they say they are.

--- script.js
+++ script.js
@@ -355,28 +355,41 @@ if (scoreJohn > scooreFriend){
 
 // Coding Challenge 2
 
-var years = [2001, 1985, 1994, 2014, 1973];
+// var years = [2001, 1985, 1994, 2014, 1973];
+//
+// function printFullAge(years) {
+//     var ages = [];
+//     var fullAges = [];
+//
+//     for (var i= 0; i < years.length; i++) {
+//         ages[i] = 2016 - years [i];
+//     }
+//     for (i = 0; i < ages.length; i++){
+//         if (ages[i] >= 18){
+//             console.log(' Person '  +  (i + 1) +  'is'  + ages  [i]  +
+//                 'years old and is of full age');
+//             fullAges.push(true);
+//         } else {
+//             console.log(' Person ' +  (i  + 1) +  'is'  +  ages  [i]  +
+//                 'years old and is NOT of full age');
+//             fullAges.push(false);
+//         }
+//     }
+//     return fullAges;
+// }
+//
+// var full_1 = printFullAge(years);
+// var full_2 = printFullAge(2012, 1915, 1999);
 
-function printFullAge(years) {
-    var ages = [];
-    var fullAges = [];
+// Functions Again
 
-    for (var i= 0; i < years.length; i++) {
-        ages[i] = 2016 - years [i];
-    }
-    for (i = 0; i < ages.length; i++){
-        if (ages[i] >= 18){
-            console.log(' Person '  +  (i + 1) +  'is'  + ages  [i]  +
-                'years old and is of full age');
-            fullAges.push(true);
-        } else {
-            console.log(' Person ' +  (i  + 1) +  'is'  +  ages  [i]  +
-                'years old and is NOT of full age');
-            fullAges.push(false);
-        }
-    }
-    return fullAges;
-}
+// function calculateAge(yearOfBirth){
+//     var age = 2016 - yearOfBirth
+//     return age;
+// }
+//
+// var  ageJohn = (calculateAge(1990));
+// console.log(ageJohn);
 
-var full_1 = printFullAge(years);
-var full_2 = printFullAge(2012, 1915, 1999);
+
+// Loops
