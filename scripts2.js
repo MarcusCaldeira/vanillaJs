8669176895f05cd05f6bd8ff5ee0804c3247a30d
Marcus Caldeira <mac.caldr@gmail.com>
2018-06-19 10:59:55 -0600

    trying to make my own function, that i can give Dylan

--- scripts2.js
+++ scripts2.js
@@ -1,18 +1,37 @@
+// console.log(this);
 
-calculateAge(1965);
+// calculateAge(1985);
+//
+// function calculateAge(year){
+//     console.log(2016-year);
+//     console.log(this);
+// }
+//
+// var john = {
+//     name: 'john',
+//     yearOfBirth: 1990,
+//     calculateAge: function () {
+//         console.log(this);
+//         console.log(2016-this.yearOfBirth);
+//         function innerFunction(){
+//             console.log(this);
+//         }
+//         innerFunction();
+//     }
+// }
+//
+// john.calculateAge();
 
-function calculateAge(year) {
-    console.log(2016-year);
+
+
+function myAge(dob) {
+    var today = new Date();
+    var birthday = new Date(dob);
+    var age = today.getYear() - birthday.getYear();
+    var marcus = today.getM() - birthday.getM();
+    if (marcus < 0 || (marcus === 0 && today.getDate() < birthday.getDate())) {
+        age = age - 1;
+    }
+    return age;
 }
-
-
-// retirement(1990);
-var retirement = function(year){
-    console.log(65 - (2016 - year));
-}
-
-
-// variables
-
-var age = 23;
-console.log(age);
+alert(getAge("09/27/1991"));
