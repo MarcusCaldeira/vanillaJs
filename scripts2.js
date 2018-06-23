5f3562b62836a6a0e09a663c39f3129d2e13c445
Marcus Caldeira <mac.caldr@gmail.com>
2018-06-23 11:21:59 -0600

    this is the html for the app itself,

--- scripts2.js
+++ scripts2.js
@@ -23,15 +23,3 @@
 // john.calculateAge();
 
 
-
-function myAge(dob) {
-    var today = new Date();
-    var birthday = new Date(dob);
-    var age = today.getYear() - birthday.getYear();
-    var marcus = today.getM() - birthday.getM();
-    if (marcus < 0 || (marcus === 0 && today.getDate() < birthday.getDate())) {
-        age = age - 1;
-    }
-    return age;
-}
-alert(age(09/27/1991));
