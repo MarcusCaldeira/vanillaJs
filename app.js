4bdf6db0ff0d0e55d1bb3cf592ed03e6d30f2e2b
Marcus Caldeira <mac.caldr@gmail.com>
2018-07-02 13:30:15 -0600

    switching the active player if the active player rolls a 1

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var scores, roundScores, activePlayer;
 
 scores = [0,0];
 roundScores  = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 
 
@@ -46,6 +46,7 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         document.querySelector('#current-' + activePlayer).textContent = roundScores;
     } else{
         //next player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
     }
 
 
@@ -55,9 +56,7 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 
 
 //Saving for LATER
-
+// document.querySelector('#current-' + activePlayer).textContent = dice;
 // document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-
-
 // var x = document.querySelector('#score-0').textContent;
 // console.log(x);
