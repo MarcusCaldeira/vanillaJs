b9a16414e74991dc39294a5ae8766bedd6cf1c70
Marcus Caldeira <mac.caldr@gmail.com>
2018-07-02 13:22:12 -0600

    roundscore =+ dice

--- app.js
+++ app.js
@@ -40,8 +40,9 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     diceDom.style.display = 'block';
     diceDom.src = 'dice-' + dice + '.png';
     // 3. Update the Round Score IF the rolled number was  not 1
-    if (dice > 1){
+    if (dice !== 1){
         //add score
+        roundScores += dice;
     } else{
         //next player
     }
