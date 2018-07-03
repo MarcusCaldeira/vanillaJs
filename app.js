81a30bf22ee60b77e7332befa63df114979e660e
Marcus Caldeira <mac.caldr@gmail.com>
2018-07-03 12:49:15 -0600

    this is the completion of the app, the game of pig is now ready to play on my server.

--- app.js
+++ app.js
@@ -91,6 +91,9 @@ function init() {
     document.querySelector('.player-1-panel').classList.remove('winner');
     document.querySelector('.player-0-panel').classList.remove('active');
     document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.add('active');
+
+
 
 
 
